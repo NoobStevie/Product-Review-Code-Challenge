4e52bb745ba51ab4a8bd13ca8e9b4c3377006122
Stevie <kamaustevek78@gmail.com>
2023-06-04 16:51:18 +0300

    Reviews Added

--- db/migrate/20230604124617_create_reviews.rb
+++ db/migrate/20230604124617_create_reviews.rb
@@ -1,4 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.integer :star_rating
+      t.string :comment
+      t.references :user, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
